ef5b4b9265a45bd83840546d11d8069e5cd68e33
ezequielformaggio <formaggio64@gmail.com>
2023-08-19 11:44:07 -0300

    agrego el if

--- tp1ejemplo.py
+++ tp1ejemplo.py
@@ -1,3 +1,10 @@
+import random
 print("esto es un ejemplo")
 
 print("hola soy gaston")
+
+numero=random.random()
+if (numero > 0.5) :
+    print("el numero es mayor a 0,5 :", numero, " nuevo texto")
+else : 
+    print("el numero es menor")
