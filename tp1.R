2bdcee90e0997e12f106bf95804f70a79549a7f7
colomboromina <romi.colombo23@gmail.com>
2023-08-20 11:40:14 -0300

    ejemplo 2

---
+++ tp1.R
@@ -0,0 +1,10 @@
+# ejemplo2
+
+# idem ejemplo 1 pero usando comando print y scan
+
+
+print('entre un valor para a (presione enter para terminar): '); a=scan()
+print('entre otro valor para b: '); b=scan()
+c=a+b
+print('la suma de a+b es')
+print(c)
